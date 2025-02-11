d20a54f4a04734f3316caf01d80981e6e735ece0
Claudio <crdevelopers506@gmail.com>
2025-02-11 14:56:35 -0600

    .

--- src/components/Navigation/NavigationHandler.tsx
+++ src/components/Navigation/NavigationHandler.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import Box from '@mui/material/Box';
